6fe942a7da7d7b231752fb4992935fd08d52dbd9
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-11-29 16:54:53 +0530

    modify the app.js

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link,NavLink } from 'react-router-dom';
+import About from './Components/About';
+import ContactUs from './Components/ContactUs';
+import Dashbord from './Components/Dashbord';
+import Home from './Components/Home';
+import Login from './Components/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+
+        <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<ContactUs/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/dash' element={<Dashbord/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
 
-export default App;
+export default Ap
